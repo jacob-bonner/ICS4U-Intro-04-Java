7fda1c1a44419aea2d07eeb9280fbabd91c0bbfe
Ubuntu <ubuntu@ip-172-31-15-206.ec2.internal>
2020-11-18 18:42:43 +0000

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created By: Jacob Bonner
+ * Created On: November 2020
+ * Runs the "Hello, World!" program in java
+ * Class HelloWorld.
+ */
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
